6546d6d93ab2431d4bc0d70d7648dfbdf3153f4e
Army <jarupa310@gmail.com>
2021-10-22 22:40:22 -0500

    Agregando variable STATIC_ROOT a settings-prod

--- gamingtv/gamingtv/settings-prod.py
+++ gamingtv/gamingtv/settings-prod.py
@@ -126,14 +126,8 @@ STATIC_URL = '/static/'
 ASGI_APPLICATION = "gamingtv.asgi.application"
 
 STATIC_ROOT = BASE_DIR / 'static'
-SASS_PROCESSOR_ROOT = STATIC_ROOT
 
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'sass_processor.finders.CssFinder',
-]
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
